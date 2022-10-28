6b7382c1faf2a0210246f0b95f1212523299b121
PeerGynt79 <79arturl79@gmail.com>
2022-10-28 15:34:56 +0300

    hw8 done

--- homework_8 copy/scripts/scripts.js
+++ homework_8 copy/scripts/scripts.js
@@ -1,78 +1,35 @@
 'use strict';
-let operations = new Set(['+','-','*','/']);
-let errorSet = new Set([NaN,Infinity,null]);
-let operand1=String(document.getElementById('operand1').value).replaceAll(' ','');
-let operand2=String(document.getElementById('operand2').value).replaceAll(' ','');
-let operation=String(document.getElementById('operation').value).replaceAll(' ','');
-//document.getElementById('operand1').addEventListener('change' , (event) =>{document.getElementById("operand1").click()})
-
-document.getElementById('result').addEventListener('click' , (event) => 
-{
-
-let operand1_error='';
-let operand2_error='';
-let operation_error='';
-event.preventDefault();       
-if  (operand1 === '') 
-    {operand1_error='Первое число не указано. '}   
-    else  
-    if (errorSet.has(Number(operand1))) 
-        {operand1_error='Некорректный ввод 1-го числа. '};
-if  (operand2 === '') 
-    {operand2_error='Второе число не указано. '}   
-    else  
-    if (errorSet.has(Number(operand2))) 
-        {operand2_error='Некорректный ввод 2-го числа. ';}
-if  (operation==='') 
-    {operation_error='Не введен знак операции.'}
-    else 
-    if (!((operation.length===1)&&(operations.has(operation)))) 
-        {operation_error='Программа не поддерживает такую операцию.'};
-let Error=(operand1_error+operand2_error+operation_error);
-if (Error) 
-    {
-        document.getElementById('result').setAttribute('style','font-size: '+Math.min((Math.round(1.5*(55/(`Результат: ${Error}`+'px').length)*10)/10),1.45)+'rem;');
-        document.getElementById('result').value=Error;
+let operation='';
+let operand1=0.0;
+let operand2=0.0;
+let resultat=0.0;
+document.getElementById('result').addEventListener('click' , (event)=>{
+    operand1 = document.getElementById('operand1').value;
+    operand2 = document.getElementById('operand2').value;
+    operation = document.getElementById('operation').value;
+    if (operand1.length===0) {
+        console.log('Первое число не указано.')   
+    } else if (!((Number(operand1)*0) === 0) ) {
+        console.log('Некорректный ввод 1-го числа.')    
+    };
+    if (operand2.length===0) {
+        console.log('Второе число не указано.')   
+    } else if (!((Number(operand2)*0) === 0 )) {
+        console.log('Некорректный ввод 2-го числа.')    
+    };
+    if (operation.length===0) {
+        console.log('Не введен знак операции.')   
+    } else if (!((operation === '/')||(operation === '*')||(operation === '+')||(operation === '-'))) {
+        console.log('Программа не поддерживает такую операцию.')    
     }
-    else document.getElementById('result').setAttribute('style','font-size: 1.5rem;');
-if (!Error) 
-    {switch (operation) 
-        {
-        case '*': 
-            {
-                let resVar=Number(operand1)*Number(operand2);
-                document.getElementById('result').value = `Результат: ${resVar}`;
-                break;
-            }
-        case '/': 
-            {
-                if (Number(operand2)===0) 
-                    {
-                    document.getElementById('result').value = 'Делить на ноль нельзя!';
-                    break;
-                    } 
-                    else 
-                    {
-                    let resVar=Number(operand1)/Number(operand2);
-                    document.getElementById('result').value = `Результат: ${resVar}`;
-                    break;
-                    }
-            }
-        case '-': 
-            {
-                let resVar=Number(operand1)-Number(operand2);
-                document.getElementById('result').value = `Результат: ${resVar}`;
-                break;
-            };
-        case '+': 
-            {
-                let resVar=Number(operand1)+Number(operand2);
-                document.getElementById('result').value = `Результат: ${resVar}`;
-                break;
-            };
-        }
+    switch (operation){ 
+        case '*': resultat = Number(operand1)*Number(operand2);break;
+        case '/': resultat = Number(operand1)/Number(operand2);break;
+        case '-': resultat = Number(operand1)-Number(operand2);break;
+        case '+': resultat = Number(operand1)+Number(operand2);break;
     }
+    if ((resultat*0) === 0 ) {console.log(resultat)
+    } else {
+    console.log('Результат невозможно вычислить!')
+    };
 });
-
-
- 
--- homework_8/scripts/scripts.js
+++ homework_8/scripts/scripts.js
@@ -5,75 +5,62 @@ document.getElementById('operand1').addEventListener('input' , (event) =>{docume
 document.getElementById('operand2').addEventListener('input' , (event) =>{document.getElementById('result').click()})
 document.getElementById('operation').addEventListener('input' , (event) =>{document.getElementById('result').click()})
 
-document.getElementById('result').addEventListener('click' , (event) => 
-{
-var operand1=String(document.getElementById('operand1').value).replaceAll(' ','');
-var operand2=String(document.getElementById('operand2').value).replaceAll(' ','');
-var operation=String(document.getElementById('operation').value).replaceAll(' ','');
-let operand1_error='';
-let operand2_error='';
-let operation_error='';
-event.preventDefault();       
-if  (operand1 === '') 
-    {operand1_error='Первое число не указано. '}   
-    else 
-    if (errorSet.has(Number(operand1))) 
-        {operand1_error='Некорректный ввод 1-го числа. '};
-        
-if  (operand2 === '') 
-    {operand2_error='Второе число не указано. '}   
-    else  
-    if (errorSet.has(Number(operand2))) 
-        {operand2_error='Некорректный ввод 2-го числа. ';}
-if  (operation==='') 
-    {operation_error='Не введен знак операции.'}
-    else 
-    if (!((operation.length===1)&&(operations.has(operation)))) 
-        {operation_error='Программа не поддерживает такую операцию.'};
-let error=(operand1_error+operand2_error+operation_error);
-if (error) 
-    {
-        document.getElementById('result').setAttribute('style','font-size: '+Math.min((Math.round(1.5*(55/(`Результат: ${Error}`+'px').length)*10)/10),1.45)+'rem;');
-        document.getElementById('result').value=error;
-    }
-    else document.getElementById('result').setAttribute('style','font-size: 1.5rem;');
-if (!error) 
-    {switch (operation) 
-        {
-        case '*': 
-            {
-                let resVar=Number(operand1)*Number(operand2);
+document.getElementById('result').addEventListener('click' , (event) => {
+    const operand1 = String(document.getElementById('operand1').value).replaceAll(' ','');
+    const operand2 = String(document.getElementById('operand2').value).replaceAll(' ','');
+    const operation = String(document.getElementById('operation').value).replaceAll(' ','');
+    let operand1Error = '';
+    let operand2Error = '';
+    let operationError = '';
+    event.preventDefault();       
+    if  (operand1 === '') {
+        operand1Error='Первое число не указано. ';
+    } else if (errorSet.has(Number(operand1))) {
+        operand1Error='Некорректный ввод 1-го числа. ';
+    };
+    if  (operand2 === '') {
+        operand2Error = 'Второе число не указано. ';
+    } else if (errorSet.has(Number(operand2))) {
+        operand2Error = 'Некорректный ввод 2-го числа. ';
+    };
+    if  (operation === '') {
+        operationError = 'Не введен знак операции.';
+    } else if (!((operation.length === 1) && (operations.has(operation)))) {
+        operationError = 'Программа не поддерживает такую операцию.';
+    };
+    let error = (operand1Error + operand2Error + operationError);
+    if (error) {
+        document.getElementById('result').setAttribute('style','font-size: ' + Math.min((Math.round(1.5*(55/(`Результат: ${Error}` + 'px').length) * 10) / 10), 1.45) + 'rem;');
+        document.getElementById('result').value = error;
+    }   else {
+        document.getElementById('result').setAttribute('style','font-size: 1.5rem;');
+    };
+    let resVar =0.0;
+    if (!error) {
+        switch (operation) {
+            case '*': 
+                resVar=Number(operand1) * Number(operand2);
                 document.getElementById('result').value = `Результат: ${resVar}`;
                 break;
-            }
-        case '/': 
-            {
-                if (Number(operand2)===0) 
-                    {
+            case '/': 
+                if (Number(operand2) === 0) {
                     document.getElementById('result').value = 'Делить на ноль нельзя!';
                     break;
-                    } 
-                    else 
-                    {
-                    let resVar=Number(operand1)/Number(operand2);
-                    document.getElementById('result').value = `Результат: ${resVar}`;
-                    break;
-                    }
-            }
-        case '-': 
-            {
-                let resVar=Number(operand1)-Number(operand2);
+                } else {
+                resVar=Number(operand1) / Number(operand2);
                 document.getElementById('result').value = `Результат: ${resVar}`;
                 break;
-            };
-        case '+': 
-            {
-                let resVar=Number(operand1)+Number(operand2);
+                }
+            case '-': 
+                resVar=Number(operand1) - Number(operand2);
                 document.getElementById('result').value = `Результат: ${resVar}`;
                 break;
-            };
-        }
-    }
+            case '+': 
+                resVar=Number(operand1) + Number(operand2);
+                document.getElementById('result').value = `Результат: ${resVar}`;
+                break;
+        };
+    };
 });
 
 
