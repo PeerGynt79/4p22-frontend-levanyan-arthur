b82a18eec5c2c2e5fa22ac276f6deff55cd2ce1f
PeerGynt79 <79arturl79@gmail.com>
2022-11-13 14:52:46 +0300

    actuate

---
+++ exprimental/homework_10_/scripts/scripts.js
@@ -0,0 +1,36 @@
+'use strict';
+
+fetch('https://reqres.in/api/users?per_page=12')
+.then((json) => {
+  return json.json()
+})
+.then((info) => {
+  console.log('-----------'); console.log('Пункт №1:'); console.log('-----------');
+  console.log('Data loading...');
+  for (let entry of info.data){
+    console.log(`IdentNum: ${entry.id}. Full Name: ${entry.first_name} ${entry.last_name}. e-mail: ${entry.email}; avatar: ${entry.avatar}`)
+  };
+  console.log(`...${info.data.length} users data loaded.`);
+  
+  console.log('-----------'); console.log('Пункт №2:'); console.log('-----------');
+  for(let entry of info.data) {
+    console.log(entry.last_name);
+  };
+  
+  console.log('-----------'); console.log('Пункт №3:'); console.log('-----------'); 
+  for (let entry of info.data.filter((item) => {
+    return item.last_name[0] === 'F';
+  }) ){console.log(`Lastname: ${entry.last_name}; Name: ${entry.first_name}; Ident: ${entry.id}; e-mail: ${entry.email}; AvatarURL: ${entry.avatar}`)};
+  
+  console.log('-----------'); console.log('Пункт №4:'); console.log('-----------');
+  const allInString=info.data.reduce((accum, item) => {
+    return accum+=` ${item.first_name} ${item.last_name},`;
+  }, '')
+  console.log(`Наша база содержит данные следующих пользователей:${allInString.slice(0,allInString.length-1)}.`);
+  
+  console.log('-----------'); console.log('Пункт №5:'); console.log('-----------');
+  for(let entry in info.data[0]) {
+    console.log(entry);
+  };
+})
+
